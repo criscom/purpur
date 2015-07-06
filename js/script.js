8e83c72ffd06a354e07296b8c8b40a0bd36b1d80
Christoph Potzinger <criscom27@gmail.com>
2015-07-06 21:57:41 +0200

    Header adjustments

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($) {
+
+  $(document).ready(function() {
+
+      // $("div.header").append('<a href="/"><div id="logo"></div></a>');
+      // $('#header').attr('href','http://www.google.com');
+
+  }
+};
+
+
+})(jQuery, Drupal, this, this.document);
+
+  /* Dominik, 28.01.2012, linkable Logo */
